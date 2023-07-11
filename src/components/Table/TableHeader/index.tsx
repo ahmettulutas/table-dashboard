a68d779eb7dd1509b0520d63acbe77cb60218aae
ahmetulutas <ahmet.ulutas@ecozum.com>
2023-07-11 21:49:55 +0300

    bugfix: limit dropdown import bugfix

--- src/components/Table/TableHeader/index.tsx
+++ src/components/Table/TableHeader/index.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from "react";
 import { LoadingSpinner } from "~/components/UIComponents";
 
 const SearchInTable = React.lazy(() => import("~/components/UIComponents/SearchInTable"));
-const Limit = React.lazy(() => import("./LimitDropdown"));
+const Limit = React.lazy(() => import("~/components/UIComponents/LimitDropdown"));
 
 const TableHeader:FunctionComponent = () => (
   <div className="my-4 flex gap-2 justify-between items-center">
--- src/components/UIComponents/index.tsx
+++ src/components/UIComponents/index.tsx
@@ -1,3 +1,4 @@
 export { default as DarkModeToggle } from "./DarkModeToggle";
 export { default as LoadingSpinner } from "./LoadingSpinner";
 export { default as ErrorModal } from "./ErrorModal";
+export { default as LimitDropdown } from "./LimitDropdown";
